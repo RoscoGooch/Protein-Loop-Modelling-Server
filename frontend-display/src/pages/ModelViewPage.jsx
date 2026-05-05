fbc4a8adf4c0ba028e725156c2d4f7a7b8b8dbd4
RoscoGooch <rsgooch2004@outlook.com>
2026-05-05 21:58:29 +0100

    fixed small login error

--- frontend-display/src/pages/ModelViewPage.jsx
+++ frontend-display/src/pages/ModelViewPage.jsx
@@ -2,9 +2,12 @@ import ProteinModelBackend from "../components/ProteinModelBackend";
 import ModelStatsBox from "../components/ModelStatsBox";
 import { useLocation, useNavigate } from "react-router-dom";
 import {useState, useEffect} from "react";
-import TopHeader from "../components/TopHeader";
+import ForceLogin from "../components/ForceLogin";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const ModelViewPage = (data) => {
+    const {isAuthenticated} = useAuth0();
+
     const navigate = useNavigate();
 
     function goToLoad() {
@@ -16,15 +19,23 @@ const ModelViewPage = (data) => {
     console.log(modelData);
 
     const [modelReady, setModelReady] = useState(false);
-    
+
+    if(isAuthenticated == true){
     return (
-    <div id='model-display'>
-        <TopHeader/>
-        <ProteinModelBackend data={modelData} onReady={() => setModelReady(true)}/>
-        {modelReady && (<ModelStatsBox data={modelData}/>)}
-        <button onClick={goToLoad}>Change chain and code</button>
-    </div>
+        <div id='model-display'>
+            <ProteinModelBackend data={modelData} onReady={() => setModelReady(true)}/>
+            {modelReady && (<ModelStatsBox data={modelData}/>)}
+            <button onClick={goToLoad}>Change chain and code</button>
+        </div>
     )
+    }
+    else{
+        return(
+        <>
+        <ForceLogin/>
+        </>
+        )
+    }
 }
 
 export default ModelViewPage;
